41f1a7426f50fab00d0e0d8cf890eaf2527d464d
jdspell <spelljd15@gmail.com>
2019-04-01 13:55:01 -0400

    logs a user in and added restrictions based on login

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      //check the password gues against the database
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -49,13 +50,50 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+//restrict access to this endpoint to only users that provide
+//the right credentials in the headers
+server.get('/api/users', restricted, only('frodo'), (req, res) => {
+  
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+function only(username) {
+  return function(req, res, next) {
+    if(username === req.headers.username) {
+      next();
+    } else {
+      res.status(403).json({ message: `You are not ${username}` });
+    }
+  };
+}
+
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+
+  if(username && password) {
+
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+      //check the password gues against the database
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+
+  } else {
+    res.status(401).json({ message: "Please provide credentials."});
+  }
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
